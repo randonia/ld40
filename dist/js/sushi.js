fc15b16254ecc975a8404e01a5b8164004591ca4
Ryan Andonian <ryanandonian@gmail.com>
2017-12-02 16:58:42 -0800

    Migrates Step class into FoodItem

--- dist/js/sushi.js
+++ dist/js/sushi.js
@@ -76,30 +76,3 @@ class Sushi extends FoodItem {
     }
   }
 }
-
-// Helper class, kind of like an internal state machine
-class Step {
-  get complete() {
-    return this._complete;
-  }
-  constructor(opts) {
-    this._scope = opts.scope; // To be assigned if needed
-    this._actionFn = opts.actionFn; // Make this resolve a promise
-    this._completeFn = opts.completeFn; // Handles anything else
-    this._ready = true;
-  }
-  doAction(callback) {
-    if (!this._ready) {
-      console.log('Cannot do this action: not ready', this);
-      callback(ACTION_RESULTS.PENDING);
-      return;
-    }
-    this._ready = false;
-    this._actionFn().then(({ result }) => {
-      console.log('inner promise starting and completed', result);
-      callback(result);
-      this._complete = true;
-      this._completeFn();
-    });
-  }
-}
